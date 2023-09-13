5a39b41f613592600de05aceb27b4123297059a0
Julia-Mishlanova <mislanovaulia94@gmail.com>
2023-09-13 21:19:56 +0400

    Implement ILoggerManager interface by LoggerManager

--- LoggerService/LoggerManager.cs
+++ LoggerService/LoggerManager.cs
@@ -1,8 +1,29 @@
-﻿using System;
+﻿using NLog;
+using System;
 
 namespace LoggerService
 {
-    public class LoggerManager
+    public class LoggerManager : ILoggerManager
     {
+        private static readonly ILogger _logger = LogManager.GetCurrentClassLogger();
+        public void LogDebug(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public void LogError(string message)
+        {
+            _logger.Error(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            _logger.Info(message);
+        }
+
+        public void LogWarning(string message)
+        {
+            _logger.Warn(message);
+        }
     }
 }
